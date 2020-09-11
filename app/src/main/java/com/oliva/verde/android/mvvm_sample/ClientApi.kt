e8cf7183930d679df1e14a31f15ff7a92595376e
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2020-09-12 06:15:55 +0900

    APIインターフェースの追加

---
+++ app/src/main/java/com/oliva/verde/android/mvvm_sample/ClientApi.kt
@@ -0,0 +1,11 @@
+package com.oliva.verde.android.mvvm_sample
+
+import io.reactivex.Single
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface ClientApi {
+    @GET("users/{user}/repos")
+    fun getGithub(@Path("user") user : String) : Single<Response<List<UserRepos>>>
+}
