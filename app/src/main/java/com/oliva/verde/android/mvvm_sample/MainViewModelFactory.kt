858bdd5eed100f0f98ba3b552bd085686733da87
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2020-09-14 18:56:25 +0900

    リポジトリの一覧をテキストとして表示

--- app/src/main/java/com/oliva/verde/android/mvvm_sample/MainViewModelFactory.kt
+++ app/src/main/java/com/oliva/verde/android/mvvm_sample/MainViewModelFactory.kt
@@ -3,6 +3,9 @@ package com.oliva.verde.android.mvvm_sample
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
+/**
+ * ViewModelをインスタンス化する方法を定義
+ */
 class MainViewModelFactory(private val clientApiRepository: ClientApiRepository) :
     ViewModelProvider.NewInstanceFactory() {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
