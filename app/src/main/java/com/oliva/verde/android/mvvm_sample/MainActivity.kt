8e83cab0190dd8932c1a257f9d5496125cf42984
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2020-10-04 23:33:16 +0900

    レイアウトファイルの変更

--- app/src/main/java/com/oliva/verde/android/mvvm_sample/MainActivity.kt
+++ app/src/main/java/com/oliva/verde/android/mvvm_sample/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 
