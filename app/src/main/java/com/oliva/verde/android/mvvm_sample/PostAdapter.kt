d42544491eabaaccf0d08bc79474f98f01919324
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2020-10-12 23:39:40 +0900

    アダプターファイルの実装

--- app/src/main/java/com/oliva/verde/android/mvvm_sample/PostAdapter.kt
+++ app/src/main/java/com/oliva/verde/android/mvvm_sample/PostAdapter.kt
@@ -1,12 +1,32 @@
 package com.oliva.verde.android.mvvm_sample
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.LiveData
 import androidx.recyclerview.widget.RecyclerView
+import com.oliva.verde.android.mvvm_sample.databinding.ActivityMainBinding
+import com.oliva.verde.android.mvvm_sample.databinding.RecyclerItemBinding
 
 
-class PostAdapter(var postList : LiveData<List<Post>>) : RecyclerView.Adapter<PostAdapter.BindingHolder>() {
-    inner class BindingHolder(var binding: )
+class PostAdapter(var postList : List<Post>) : RecyclerView.Adapter<PostAdapter.BindingHolder>() {
+
+    inner class BindingHolder(var binding: RecyclerItemBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BindingHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = RecyclerItemBinding.inflate(layoutInflater, parent, false)
+        return BindingHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: BindingHolder, position: Int) {
+        val post = postList[position]
+        holder.binding.post = post
+    }
+
+    override fun getItemCount(): Int {
+        return postList.size
+    }
 }
