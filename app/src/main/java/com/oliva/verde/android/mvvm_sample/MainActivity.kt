d8def13ebfcb8df4f7ef26733ef71eb3da8e06a7
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2020-10-19 00:58:49 +0900

    LayoutManagerの記述追加

--- app/src/main/java/com/oliva/verde/android/mvvm_sample/MainActivity.kt
+++ app/src/main/java/com/oliva/verde/android/mvvm_sample/MainActivity.kt
@@ -6,19 +6,25 @@ import android.util.Log
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.oliva.verde.android.mvvm_sample.databinding.ActivityMainBinding
 import com.oliva.verde.android.mvvm_sample.databinding.RecyclerItemBinding
 
 class MainActivity : AppCompatActivity() {
+    private var postList : MutableList<Post> = mutableListOf()
     private lateinit var mainActivityViewModel: MainActivityViewModel
+
+    // bindingのインスタンスを取得
+    private val binding : ActivityMainBinding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+            .apply {
+                lifecycleOwner = this@MainActivity
+            }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        // bindingのインスタンスを取得
-        val binding : ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.lifecycleOwner = this
 
         // Viewの状態をViewModelが保持するためには、ViewModelProviderを使用する
         mainActivityViewModel = ViewModelProvider(
@@ -30,16 +36,19 @@ class MainActivity : AppCompatActivity() {
         mainActivityViewModel.getAllPosts()
 
         // 流し込みが完了するたびに、画面部品に変更を反映する
-        val postList : MutableList<Post> = mutableListOf()
+        for (i in 0..10) {
+            postList.add(Post("title${i}", "content${i}"))
+        }
+        Log.d("ConfirmPostList", postList.toString())
+        binding.rvPosts.adapter = PostAdapter(postList)
+        binding.rvPosts.layoutManager = LinearLayoutManager(this@MainActivity)
+        /**
         mainActivityViewModel.allPosts.observe(this, Observer {
             it.forEach { post ->
                 postList.add(post)
             }
             // binding.rvPosts.adapter = PostAdapter(postList)
         })
-        for (i in 0..99) {
-            postList.add(Post("title${i}", "content${i}"))
-        }
-        binding.rvPosts.adapter = PostAdapter(postList)
+        */
     }
 }
