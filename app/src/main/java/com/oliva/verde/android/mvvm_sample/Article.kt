c48802361fecdc7d38deebb0b21ad364962f1ca7
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2020-11-17 22:54:58 +0900

    Create Article.kt

---
+++ app/src/main/java/com/oliva/verde/android/mvvm_sample/Article.kt
@@ -0,0 +1,7 @@
+package com.oliva.verde.android.mvvm_sample
+
+data class Article(
+    val url : String = "",
+    val urlToImage : String = "",
+    val publishedAt : String = "",
+    val title : String = "")
